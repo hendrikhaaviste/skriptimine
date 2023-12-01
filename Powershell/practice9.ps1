6849fd160975b220f2b9553444a642ab179db752
hendrikhaaviste <hendrik.haaviste@voco.ee>
2023-12-01 14:36:07 +0200

    Loendab notepad protsesse

---
+++ Powershell/practice9.ps1
@@ -0,0 +1,17 @@
+﻿##Skript, mis annab teada kui kaua töötas notepad
+$ct1 = 0
+##Skript loendab protsesse ning määratud intervall 1 sekundiga
+do
+{
+$ch = $null
+$ch = Get-Process | ?{$_.name -like "note*"}
+if($ch -ne $null)
+{$ct2 = 2
+Write-Host "Notepad is running"
+Start-Sleep -Seconds 1
+$ct1++
+}
+else
+{$ct2 = 1}
+}while($ct2 -ne 1)
+$ct1
