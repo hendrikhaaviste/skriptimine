e567aaed63316c2f953765004758a369723c276e
hendrikhaaviste <hendrik.haaviste@voco.ee>
2023-12-01 14:27:09 +0200

    Tudengite grupeering värvidega

---
+++ Powershell/practice7.ps1
@@ -0,0 +1,21 @@
+﻿##Skript jaotab tundengid värvide järgi
+$groups = @("Red", "Green", "Yellow", "Blue")
+
+$result = @()
+
+##Alustame tsükliga, mis võtab kuni 20 arvu
+for ($i = 1; $i -le 20; $i++) {
+##Saame juhusliku värvivaliku grupist $groups
+    $g = Get-Random $groups
+    
+##Loome ajutise objekti, kus on tundengi rolli number ja värvi grupeering
+    $temp = [PSCustomObject]@{
+        RollNumber = $i
+        Group = $g
+    }
+    
+    $result += $temp
+}
+
+## Väljastame tulemuse
+$result
