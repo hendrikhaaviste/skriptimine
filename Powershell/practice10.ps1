a1d8b32775b504380200b359210b77fdba903476
hendrikhaaviste <hendrik.haaviste@voco.ee>
2023-12-01 14:42:45 +0200

    Loendab protsesse do-until abil

---
+++ Powershell/practice10.ps1
@@ -0,0 +1,18 @@
+﻿##Skript loendab protsesse
+$ct1 = 0
+##Skript on sisult sarnane practice 9 omaga, aga kasutab Do While asemel Do Until
+##Do Until töötab kuni tingimus on täidetud
+do
+{
+$ch = $null
+$ch = Get-Process | ?{$_.name -like "note*"}
+if($ch -ne $null)
+{$ct2 = 2
+Write-Host "Notepad is running"
+Start-Sleep -Seconds 1
+$ct1++
+}
+else
+{$ct2 = 1}
+}until($ct2 -eq 1)
+$ct1
